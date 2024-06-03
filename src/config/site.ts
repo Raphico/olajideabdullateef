74b38b58f464e779c243fa467de2230691d1e8b4
Raphico <raphicogit@gmail.com>
2024-06-03 09:09:35 +0100

    fix: correct x link

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ const links = {
   facebook: "https://www.facebook.com/Abdullateef0la",
   github: "https://github.com/TheOlajide",
   linkedin: "https://ng.linkedin.com/in/olajideabdullateef",
-  twitter: "https://x.com/TheOlajide",
+  twitter: "https://x.com/TheOlajide_",
   gmail: "mailto:olajideabdullateef@gmail.com",
   whatsapp: "https://wa.link/2lmgj9",
 }
